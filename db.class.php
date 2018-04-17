4e607b5bafcb498d1c37ec25db29eb7ea252161d
Thomas Schultz <31558370+tjs6f2@users.noreply.github.com>
2018-04-17 13:57:51 -0500

    connects database and adds data to database

---
+++ db.class.php
@@ -0,0 +1,59 @@
+<?php
+
+class db {
+    var $link;
+
+    /// Create new database connection
+    function db($database, $host = "sql104.epizy.com", $user = "epiz_21894097", $password = "PqyTPgghbKhv") {
+        $this->connect($host, $user, $password);
+        $this->select_db($database);
+    }
+
+    /// Connect to database.
+    function connect($host, $user, $password) {
+       if ($password == "PqyTPgghbKhv") {
+        $this->link = mysql_pconnect($host, $user) or die(mysql_error($this->link));
+       } 
+        else {
+	  $this->link = mysql_pconnect($host, $user, $password)
+	                or die(mysql_error($this->link));
+       }
+    }
+
+    /// Select database.
+    function select_db($database) {
+        mysql_select_db($database, $this->link) or die(mysql_error($this->link));
+    }
+
+    /// Execute SQL query.
+    function query($sql) {
+        
+        return mysql_query($sql, $this->link);
+    }
+
+    /// Return number of rows in result. num_rows
+    function numberRows($query_result) {
+        
+        return mysql_num_rows($query_result);
+    }
+
+    /// Return result as an object.
+    function fetch_object($query_result) {
+        
+        return mysql_fetch_object($query_result);
+    }
+
+    /// Free spaced used by result.
+    function free_result($query_result) {
+        
+        return mysql_free_result($query_result);
+    }
+
+    /// Seek to a given row of result.
+    function data_seek($query_result, $rowindex = 0) {
+        
+        return mysql_data_seek($query_result, $rowindex);
+    }
+}
+
+?>
