a57574ee3d451ca82c5830d259c2c70f1b8f9d90
Thomas Schultz <31558370+tjs6f2@users.noreply.github.com>
2018-04-17 13:55:35 -0500

    database credentials

---
+++ dbConf.php
@@ -0,0 +1,23 @@
+<?
+
+   $db_host="sql104.epizy.com"; //name of the database host
+   $db_user="epiz_21894097"; //database username
+   $db_passwd="PqyTPgghbKhv"; //password
+   $db_database="epiz_21894097_guestbook"; //databasename
+
+   define(customer_comments,"customer_comments");
+
+   //set this variable to FALSE if you want to display only a certain number
+   //of entries of guestbook.
+   $display_single_page=FALSE;
+   
+   //if above variable is FALSE following number of entries will be displayed at once.
+   $display=5;
+
+
+  //admin login and password for deleting entries required for page
+  // admin.php
+  $admin_login="admin";
+  $admin_password="test";
+
+?>
