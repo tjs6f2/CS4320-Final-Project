31f81ffa64f2858919b6045cc5fca26203157c3d
Thomas Schultz <31558370+tjs6f2@users.noreply.github.com>
2018-04-21 15:26:37 -0500

    formatted code / fixed typos

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@
     function admin_head(){
         print"<html>\n";
             print "<head><title>Guest Book Admin</title>\n";
-            print "<link rel=\"stylesheet\" href=\"gb.css\" type=\"text/css\">\n";
+            print "<link rel=\"stylesheet\" href=\"design.css\" type=\"text/css\">\n";
             print"<head>\n";
             print"<body>\n";
         
@@ -52,12 +52,12 @@
 		          print "<caption><div class='small1black'>Edit Entry</div></caption>\n";
 				  print "<tr bgcolor='#FFFFFF'>\n";
 				  print "   <td ALIGN='RIGHT' class='datablack' width='20%'><strong>Name</strong></td>\n";
-	              print "   <td align='left'> <input class='text' NAME='entry_author' TYPE='TEXT'  VALUE='".$admin_gb->entry_author."'";
+	              print "   <td align='left'> <input class='text' NAME='entry_author' TYPE='TEXT'  VALUE='".$admin_gb->author."'";
                   print " CLASS='text'></td>\n";
                   print "</tr>\n";
                   print "<tr bgcolor='#FFFFFF' >\n";
                   print "   <td ALIGN='RIGHT' class='datablack'> <strong>Email</strong> </td>\n";
-                  print "   <td align='left'> <input class='text' NAME='entry_email' TYPE='TEXT' VALUE='".$admin_gb->entry_email."'";
+                  print "   <td align='left'> <input class='text' NAME='entry_email' TYPE='TEXT' VALUE='".$admin_gb->email."'";
                   print " CLASS='text'></td>\n";
                   print "</tr>\n";
                   print "<tr bgcolor='#FFFFFF'>\n";
@@ -67,23 +67,23 @@
                   print "</tr>\n";
                   print "<tr bgcolor='#FFFFFF'>\n";
                   print "    <td ALIGN='RIGHT' class='datablack'><strong>Birth Date</strong></td>\n";
-                  print "    <td align='left'><input class='text' NAME='entry_dob' TYPE='TEXT'  VALUE='".$admin_gb->entry_dob."'";
+                  print "    <td align='left'><input class='text' NAME='entry_dob' TYPE='TEXT'  VALUE='".$admin_gb->dob."'";
                   print " CLASS='text'></td>\n";
                   print "</tr>\n";
                   print "<tr bgcolor='#FFFFFF'>\n";
                   print "   <td ALIGN='RIGHT' class='datablack'><strong>Location</strong> </td>\n";
-                  print "    <td align='left'><input class='text' NAME='entry_location' TYPE='TEXT'  VALUE='".$admin_gb->entry_location."'";
+                  print "    <td align='left'><input class='text' NAME='entry_location' TYPE='TEXT'  VALUE='".$admin_gb->location."'";
                   print " CLASS='text'></td>\n";
                   print "</tr>\n";
 
                   print "<tr bgcolor='#FFFFFF'>\n";
                   print "   <td ALIGN='RIGHT' class='datablack'><strong>Website</strong> </td>\n";
-                  print "    <td align='left'><input class='text' NAME='entry_website' TYPE='TEXT'  VALUE='".$admin_gb->entry_url."'";
+                  print "    <td align='left'><input class='text' NAME='entry_website' TYPE='TEXT'  VALUE='".$admin_gb->url."'";
                   print " CLASS='text'></td>\n";
                   print "</tr>\n";
                   print "<tr bgcolor='#FFFFFF'>\n";
                   print "   <td ALIGN='RIGHT' class='datablack'><strong>Comments</strong> </td>\n";
-                  print "    <td align='left'><textarea  class='text' name='entry_comments' cols='25' rows='5'>".$admin_gb->entry_comments."</textarea>";
+                  print "    <td align='left'><textarea  class='text' name='entry_comments' cols='25' rows='5'>".$admin_gb->comments."</textarea>";
                   print " </td>\n";
                   print "</tr>\n";
 
@@ -91,7 +91,7 @@
 
 
           print "</table>\n";
-	      print "<input NAME='id' TYPE='HIDDEN' VALUE='".$admin_gb->entry_id."'>\n";
+	      print "<input NAME='id' TYPE='HIDDEN' VALUE='".$admin_gb->id."'>\n";
           print "<input NAME='submit' TYPE='SUBMIT' VALUE='Update' CLASS='btn1'>\n";
 
 	      print "<input NAME='submit' TYPE='SUBMIT' VALUE='Cancel' CLASS='btn1'>\n";
@@ -103,13 +103,13 @@
 		 switch(strtolower($submit)) {
 			case "update":
 
- 				$admin_gb->entry_id=$_POST['id'];
- 		     	$admin_gb->entry_dob=$_POST['entry_dob'];
-                $admin_gb->entry_location=$_POST['entry_location'];
-				$admin_gb->entry_author=$_POST['entry_author'];
-				$admin_gb->entry_email=$_POST['entry_email'];
-				$admin_gb->entry_url=$_POST['entry_website'];
-				$admin_gb->entry_comments=$_POST['entry_comments'];
+ 				$admin_gb->id=$_POST['id'];
+ 		     	$admin_gb->dob=$_POST['entry_dob'];
+                $admin_gb->location=$_POST['entry_location'];
+				$admin_gb->author=$_POST['entry_author'];
+				$admin_gb->email=$_POST['entry_email'];
+				$admin_gb->url=$_POST['entry_website'];
+				$admin_gb->comments=$_POST['entry_comments'];
 				$admin_gb->referer=$_POST['entry_referer'];
 				if($admin_gb->modify_entry()) {
 					$message="Selected Entry Modified Succesfully";
@@ -132,50 +132,51 @@
             
             
             
-	   $sql = "Select * from ".ENTRY_TABLE." order by entry_date DESC";
+	   $sql = "Select * from ".customer_comments." order by entry_date DESC";
 	   $result=$db->query($sql);
 
         if($admin_gb->total >0)
 		           {
 
-						print "<form action='admin.php' method='post'>\n";
-						print "<table BORDER=0 CELLSPACING=1 CELLPADDING=3 class='admin_list'>\n";
+				print "<form action='admin.php' method='post'>\n";
+				print "<table BORDER=0 CELLSPACING=1 CELLPADDING=3 class='admin_list'>\n";
 		                print "    <tr class='admin_list_top'><!-- header -->\n";
 			            print "       <td ALIGN='CENTER' >&nbsp;</td>\n";
-				        print "       <td ALIGN='CENTER' NOWRAP>&nbsp;</td>\n";
-						print "       <td>Name</td>\n";
+				    print "       <td ALIGN='CENTER' NOWRAP>&nbsp;</td>\n";
+				    print "       <td>Name</td>\n";
 			            print "       <td>Email</td>\n";
 			            print "       <td >Location</td>\n";
 			            print "       <td ALIGN='CENTER' NOWRAP >Date Signed</td>\n";
-				        print "       <td>Comments</td>\n";
-						print "       <td> IP Address</td>\n";
+				    print "       <td>Comments</td>\n";
+				    print "       <td> IP Address</td>\n";
 		                print "    </tr>\n";
-				        while($data=mysql_fetch_object($result)) {
+				        
+		                             while($data=mysql_fetch_object($result)) {
 
 							print "    <tr class='admin_list_data'>\n";
 							print "       <td  ALIGN='CENTER'>\n";
-							print "           <input NAME='entryids[]' TYPE='CHECKBOX' VALUE='".$data->entry_id."'>\n";
+							print "           <input NAME='entryids[]' TYPE='CHECKBOX' VALUE='".$data->id."'>\n";
 		                    print "       </td>\n";
 				            print "       <td  ALIGN='CENTER' VALIGN='middle' NOWRAP>\n";
-						    print "          <a HREF='".$_SERVER['PHP_SELF']."?action=edit&edit=".$data->entry_id."' class='small1'>Edit</a>\n";
+						    print "          <a HREF='".$_SERVER['PHP_SELF']."?action=edit&edit=".$data->id."' class='small1'>Edit</a>\n";
 		                    print "       </td>\n";
 				            print "       <td NOWRAP class='datablack'>\n";
-                            print($data->entry_author);
+                            print($data->author);
 						 	print "  	 </td>\n";
   							print "       <td   class='datablack'>\n";
-	                        print($data->entry_email);
+	                        print($data->email);
 			                print "       </td>\n";
 				          	print "       <td   ALIGN='CENTER' VALIGN='TOP' NOWRAP class='datablack'>\n";
-	                        print($data->entry_location);
+	                        print($data->location);
 						    print "       </td>\n";
 			                print "       <td ALIGN='CENTER' VALIGN='TOP' NOWRAP class='datablack'>\n";
-	                        print($data->entry_date);
+	                        print($data->date_submitted);
 				            print " 	     </td>\n";
  						    print "       <td  class='datablack' >\n";
-	                        print($data->entry_comments);
+	                        print($data->comments);
 		 	                print "       </td>\n";
 			                print "       <td class='datablack' >\n";
-                            print($data->entry_ip);
+                            print($data->ip);
 					        print "       </td>\n";
 				            print "    </tr>\n";
 					    }
@@ -184,7 +185,6 @@
 						print "</form>\n";
 
 					}else {
-						//if there is no entry in the guestbook
 						print "<div class='error'>The guestbook is empty.";
          		    }
 
@@ -193,26 +193,29 @@
 
 			if($HTTP_POST_VARS['submit']=="Login") {
 			    $login=$HTTP_POST_VARS['login'];
-                $password=$HTTP_POST_VARS['password'];
-                if($login==$admin_login && $password==$admin_password) {
-                    @session_start();
-		            $_SESSION['login']="$login";
-		            header("Location:admin.php?code=1");
-                 } else {
+                            $password=$HTTP_POST_VARS['password'];
+                                
+				if($login==$admin_login && $password==$admin_password) {
+                                    @session_start();
+		                    $_SESSION['login']="$login";
+		                    header("Location:admin.php?code=1");
+                        } 
+				else {
 				     header("Location:admin.php?code=4");
 				 }
 
 
-        }  else {
+        }  
+	    else {
 
 
                 admin_head();
 				if(isset($code) && $code==4)
 					print("<font color='red'><b>Invalid Id/Password </b></font>");
 				print "<form action='admin.php' method='post'>\n";
-                print "    <table BORDER=0 CELLSPACING=0 CELLPADDING=3 CLASS='tbl2' align='left'>\n";
-                print "    <caption><font SIZE=-1>Sign In To GuestBook Admin</font></caption>\n";
-                print "    <tr>\n";
+                    print "    <table BORDER=0 CELLSPACING=0 CELLPADDING=3 CLASS='tbl2' align='left'>\n";
+                    print "    <caption><font SIZE=-1>Sign In To GuestBook Admin</font></caption>\n";
+                    print "    <tr>\n";
 	            print "        <td><font SIZE=-2>USER ID:</font></td>\n";
 	            print "        <td><input NAME='login' TYPE='TEXT'></td>\n";
 	            print "    </tr>\n";
@@ -232,8 +235,6 @@
 
 
 print "</body></html>";
-
-
 
 ?>
  
